01cbb068357d42d484d332aa090f9a4535a122a0
rohan <rohan@creativelifeform.com>
2017-03-08 14:16:33 +1100

    Moving towards a configurable way to automatically have assets pulled up from the src directory to the app directory so that coding can occur within src while hotloading works

--- template/app/utils/watch.js
+++ template/app/utils/watch.js
@@ -1,11 +1,70 @@
-const watch = (src) => {
-    let fs = require('fs');
-    let task = fs.watch(src, () => {
-        if (!src || src === '') {
-            return;
-        }
+var WatchTower = class WatchTower {
+    constructor() {
+        this.fs = require('fs-extra');
+        this.fs.readJson('package.json', (err, config) => {
+            console.log(err);
+            this.config = config;
+            this.watchSrcCss();
+            this.watchSrcJs();
+            this.watchSrcMedia();
+            this.watchAppCss();
+            this.watchAppJs();
+        });
 
+        return this;
+    }
+
+    hotload(task) {
+        console.log('hotload');
         location.reload();
         task.close();
-    });
+    }
+
+    watchSrcCss() {
+        let task = this.fs.watch(this.config.assets.css, (eventType, filename) => {
+            this.fs.copy(
+                this.config.assets.css + filename,
+                "assets/css/app.css",
+                (err) => {
+                    console.log(err);
+                }
+            );
+        });
+    }
+
+    watchSrcJs() {
+        let task = this.fs.watch(this.config.assets.js, (eventType, filename) => {
+            this.fs.copy(
+                this.config.assets.js + filename,
+                "assets/js/app.js",
+                (err) => {
+                    console.log(err);
+                }
+            )
+        });
+    }
+
+    watchSrcMedia() {
+        let task = this.fs.watch(this.config.assets.media, (eventType, filename) => {
+            this.fs.copy(
+                this.config.assets.media,
+                "assets/",
+                (err) => {
+                    console.log(err);
+                }
+            )
+        });
+    }
+
+    watchAppCss() {
+        let task = this.fs.watch("assets/css/app.css", (eventType, filename) => {
+            this.hotload(task);
+        });
+    }
+
+    watchAppJs() {
+        let task = this.fs.watch("assets/js/app.js", (eventType, filename) => {
+            this.hotload(task);
+        });
+    }
 }
