3b1c50ba93059bb95b515130f76e2b698ee11141
rohan <rohan@creativelifeform.com>
2017-03-10 07:41:38 +1100

    watcher working for filesystem files and directories now, next step is to get it to read remote files

--- template/app/utils/watcher.js
+++ template/app/utils/watcher.js
@@ -1,95 +1,107 @@
-module.exports = class watcher {
-    constructor() {
-        this.fs = require('./fs.js');
-        this.map = null;
+const watcher = class watcher {
+    constructor(config = 'package.json') {
+        this.fs = require('fs-extra');
+        this.watch = require('glob-watcher');
+        this.path = require('path');
 
-        return this;
-    }
+        this.fs.readJson(config, (err, contents) => {
+            if (err) return console.log(err);
 
-    watch(config = 'package.json') {
-        fs.readJson(config, (err, contents) => {
-            let src, app, map;
+            this.src = contents.assets.src;
+            this.app = contents.assets.app;
 
-            if (err) {
-                return console.log(err);
-            }
-
-            src = contents.assets.src;
-            app = contents.assets.app;
-            map = {
-                css: {
-                    path: `${app}/css/`,
-                    name: "app.css",
-                },
-
-                js: {
-                    path: `${app}/js/`,
-                    name: "app.js",
-                },
-
-                media: {
-                    path: `${app}`
-                }
-            };
-
-            this.setMap(map)
-                .watchSrc(src)
-                .watchApp(app);
-        });
-    }
-
-    setMap(map) {
-        this.map = map;
-
-        return this;
-    }
-
-    watchSrc(src) {
-        let cssWatcher = this.fs.watch(src.css, (eventType, filename) => {
-            this.fs.copy(
-                src.css + filename,
-                this.map.css.path + this.map.css.name,
-                (err) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                }
-            );
-        });
-
-        let jsWatcher = this.fs.watch(src.js, (eventType, filename) => {
-            this.fs.copy(
-                src.js + filename,
-                this.map.js.path + this.map.js.name,
-                (err) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                }
-            );
-        });
-
-        let mediaWatcher = this.fs.watch(src.media, (eventType, filename) => {
-            this.fs.copy(
-                src.media,
-                this.map.media.path,
-                (err) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                }
-            );
+            this.watchSrc().watchApp();
         });
 
         return this;
     }
 
-    watchApp(assetsDir) {
-        let task = this.fs.watch(assetsDir, (eventType, filename) => {
-            location.reload();
-            task.close();
+    /**
+     * Watches a file and copies it to the destination.
+     *
+     * @param {string} file - the location of the file to watch
+     * @param {string} dest - the destination to copy the file to
+     */
+    watchFile(file, dest) {
+        if (!file || file === '') {
+            return;
+        }
+
+        this.watch(file).on('change', (path, stat) => {
+            this.fs.copy(path, dest, (err) => {
+                if (err) console.log(err);
+            });
+        });
+    }
+
+    /**
+     * Watches a directory and copies it and all of its contents to the destination.
+     *
+     * @param {string} dir - the directory to watch
+     * @param {string} dest - the destination to copy to
+     * @return void
+     */
+    watchDir(dir, dest) {
+        let rel, dirname;
+
+        if (!dir || dir === '') {
+            return;
+        }
+
+        this.watch(dir).on('change', (path, stat) => {
+            rel = this.path.dirname(path);
+            dirname = rel.split('/').pop();
+
+            this.fs.ensureDir(dest + dirname, (err) => {
+                if (err) console.log(err);
+            });
+
+            this.fs.copy(dir, dest + dirname, (err) => {
+                if (err) console.log(err);
+            });
+        })
+    }
+
+    /**
+     * Watches the src and copies its files and directories to their required app locations.
+     *
+     * @return {object} watcher
+     */
+    watchSrc() {
+        let app = this.app;
+        let dest = {
+            css: `${app.dir}${app.css.path}${app.css.namespace}.css`,
+            js: `${app.dir}${app.js.path}${app.js.namespace}.js`,
+            media: `${app.dir}/`
+        }
+        let css = this.watchFile(this.src.bundles.css, dest.css);
+        let js = this.watchFile(this.src.bundles.js, dest.js);
+        let media = this.watchDir(this.src.media, dest.media);
+
+        return this;
+    }
+
+    /**
+     * Watches the provided app directory and calls the hotload method.
+     *
+     * @return {object} watcher
+     */
+    watchApp() {
+        let task = this.fs.watch(this.app.dir, (eventType, filename) => {
+            this.hotload(task);
         });
 
         return this;
+    }
+
+    /**
+     * Reloads the location.
+     *
+     * @param {object} task - the task that caused the reload
+     * @return void
+     */
+    hotload(task) {
+        location.reload();
+        task.close();
     }
 }
--- update/app/utils/watcher.js
+++ update/app/utils/watcher.js
@@ -1,83 +1,105 @@
 const watcher = class watcher {
-    constructor() {
+    constructor(config = 'package.json') {
         this.fs = require('fs-extra');
-        this.map = null;
+        this.watch = require('glob-watcher');
+        this.path = require('path');
 
-        return this;
-    }
-
-    watch(config = 'package.json') {
         this.fs.readJson(config, (err, contents) => {
-            if (err) {
-                return console.warn(err);
-            }
+            if (err) return console.log(err);
 
             this.src = contents.assets.src;
             this.app = contents.assets.app;
 
-            this.watchSrc()
-                .watchApp();
-        });
-    }
-
-    watchSrc() {
-        let src = this.src;
-        let app = this.app;
-        let cssWatcher = this.fs.watch(src.dir + src.css, (eventType, filename) => {
-            this.fs.copy(
-                src.dir + src.css,
-                app.css.path + app.css.name,
-                (err) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                }
-            );
-        });
-
-        let jsWatcher = this.fs.watch(src.js, (eventType, filename) => {
-            this.fs.copy(
-                src.js,
-                this.map.js.path + this.map.js.name,
-                (err) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                }
-            );
-        });
-
-
-        /**
-         * Copies the entire src 'media' directory to the app 'assets' directory.
-         * Necessary as CSS will reference a specific structure for images/fonts etc.,
-         *
-         */
-        let mediaWatcher = this.fs.watch(src.media, (eventType, filename) => {
-            this.fs.ensureDir(this.)
-            this.fs.copy(
-                src.media,
-                this.app,
-                (err) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                }
-            );
+            this.watchSrc().watchApp();
         });
 
         return this;
     }
 
-    watchApp() {
+    /**
+     * Watches a file and copies it to the destination.
+     *
+     * @param {string} file - the location of the file to watch
+     * @param {string} dest - the destination to copy the file to
+     */
+    watchFile(file, dest) {
+        if (!file || file === '') {
+            return;
+        }
+
+        this.watch(file).on('change', (path, stat) => {
+            this.fs.copy(path, dest, (err) => {
+                if (err) console.log(err);
+            });
+        });
+    }
+
+    /**
+     * Watches a directory and copies it and all of its contents to the destination.
+     *
+     * @param {string} dir - the directory to watch
+     * @param {string} dest - the destination to copy to
+     * @return void
+     */
+    watchDir(dir, dest) {
+        let rel, dirname;
+
+        if (!dir || dir === '') {
+            return;
+        }
+
+        this.watch(dir).on('change', (path, stat) => {
+            rel = this.path.dirname(path);
+            dirname = rel.split('/').pop();
+
+            this.fs.ensureDir(dest + dirname, (err) => {
+                if (err) console.log(err);
+            });
+
+            this.fs.copy(dir, dest + dirname, (err) => {
+                if (err) console.log(err);
+            });
+        })
+    }
+
+    /**
+     * Watches the src and copies its files and directories to their required app locations.
+     *
+     * @return {object} watcher
+     */
+    watchSrc() {
         let app = this.app;
-        let task = this.fs.watch(app, (eventType, filename) => {
+        let dest = {
+            css: `${app.dir}${app.css.path}${app.css.namespace}.css`,
+            js: `${app.dir}${app.js.path}${app.js.namespace}.js`,
+            media: `${app.dir}/`
+        }
+        let css = this.watchFile(this.src.bundles.css, dest.css);
+        let js = this.watchFile(this.src.bundles.js, dest.js);
+        let media = this.watchDir(this.src.media, dest.media);
+
+        return this;
+    }
+
+    /**
+     * Watches the provided app directory and calls the hotload method.
+     *
+     * @return {object} watcher
+     */
+    watchApp() {
+        let task = this.fs.watch(this.app.dir, (eventType, filename) => {
             this.hotload(task);
         });
 
         return this;
     }
 
+    /**
+     * Reloads the location.
+     *
+     * @param {object} task - the task that caused the reload
+     * @return void
+     */
     hotload(task) {
         location.reload();
         task.close();
